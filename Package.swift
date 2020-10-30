c31ffbfc9e72be0e3e5fc53215f24e7963744f75
Dmytro Shapovalov <dshtorm@gmail.com>
2020-10-30 15:45:04 +0100

    Release 4.6.0-rc.1

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let name = "HyperTrack"
-let version = "4.5.1"
+let version = "4.6.0-rc.1"
 
 let package = Package(
   name: name,
@@ -13,7 +13,7 @@ let package = Package(
     .binaryTarget(
       name: name,
       url: "https://github.com/hypertrack/sdk-ios/releases/download/\(version)/\(name).xcframework.zip",
-      checksum: "5ddbb3100164e2f4d5915ad0cffbb0c0b4dd9979e3b28ee2ab406efbe0007355"
+      checksum: "6d8bff30c0b8e45f98221050e4bf6782661a1876e8e9b8ac53e04de88224c244"
     )
   ],
   swiftLanguageVersions: [.v5]
